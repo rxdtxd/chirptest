e95056368b93df1ea0d72cc5e529ed34f88cd90a
rxdtxd <rxdtxd@wemakethings.net>
2016-04-14 18:49:07 +0300

    squash

--- src/main.c
+++ src/main.c
@@ -12,17 +12,20 @@
 #include "uart.h" // debug terminal
 
 
-int main (void) {    
+int main (void) {
+    uint8_t i = 0;
+    
     led_init();
 //    i2c_init();
-
+    
     // debug
     uart_init();
     stdout = &uart_output;
 
     while (1) {
-	printf("hw\n");
-	_delay_us(1.0);
+	printf("i: %u\n", i);
+	i++;
+	_delay_ms(1.0);
     }
     
     return 0;
