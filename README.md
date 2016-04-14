58693af1017c06e32e3fc60aa69a49df3ccb7120
rxdtxd <rxdtxd@wemakethings.net>
2016-04-14 18:37:05 +0300

    Hello, world!

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# arduino+chirpsensor
+# chirptest
 
 ## Intro
 
--- src/main.c
+++ src/main.c
@@ -1,23 +1,29 @@
 // main.c
-// arduino+chirpsensor
+// chirptest
 
 #include "pinout.h"
 
 #include <stdbool.h>
 #include <util/delay.h>
+#include <util/twi.h>   // i2c
 
-#include "i2c.h"
+//#include "i2c.h"
 #include "iocontrol.h"
-/* #include "uart.h" // debug terminal */
+#include "uart.h" // debug terminal
 
 
 int main (void) {    
     led_init();
-    i2c_init();
+//    i2c_init();
 
-    /* // debug */
-    /* uart_init(); */
-    /* stdout = &uart_output; */
+    // debug
+    uart_init();
+    stdout = &uart_output;
 
+    while (1) {
+	printf("hw\n");
+	_delay_us(1.0);
+    }
+    
     return 0;
 }
