dd973fca63143ab71f819dce113a530f5079a6c8
rxdtxd <rxdtxd@wemakethings.net>
2016-04-14 21:54:04 +0300

    WIP squash

--- src/main.c
+++ src/main.c
@@ -11,7 +11,12 @@
 #include "uart.h" // debug terminal
 
 // http://wemakethings.net/chirp/
-#define CHIRP_ADDR 0x20
+#define CHIRP_ADDR    0x20
+
+// i2c 7-bit addressing shit
+#define CHIRP_WRITE   (CHIRP_ADDR<<1)
+#define CHIRP_READ    (CHIRP_ADDR<<1)&1
+
 #define CHIRP_CAP_REG 0
 #define CHIRP_DATALEN 2
 
@@ -29,13 +34,14 @@ int main (void) {
 
     while (1) {
 	printf("debug start\n");
-	i2c_start(CHIRP_ADDR);
-	printf("debug write\n");
+	i2c_start(CHIRP_WRITE);
 	i2c_write(CHIRP_CAP_REG);
-	printf("debug stop\n");
 	i2c_stop();
+	printf("debug stopped\n");
+	
 	printf("debug receive\n");
 	i2c_receive(CHIRP_ADDR, data, CHIRP_DATALEN);
+	
 	printf("i: %u; data[0]: %u; data[1]: %u\n", i, data[0], data[1]);
 	
 	i++;
